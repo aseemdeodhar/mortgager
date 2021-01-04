6c5747469dfde6596257285862c097ccf127c0ed
Aseem Deodhar <39370828+aseemdeodhar@users.noreply.github.com>
2021-01-04 12:08:09 -0500

    Add files via upload

--- R/min_income.R
+++ R/min_income.R
@@ -2,6 +2,8 @@
 #'
 #' Minimum annual income required for total housing costs to remain at or below 30% of household income:
 #'
+#' * To be deprecated. Use reqd_income() instead *
+#'
 #' @param monthlypayment Total Monthly payments towards home-ownership
 #'
 #' @return Minimum Annual Income required to keep \code{monthlypayment} at or below 30% of income
